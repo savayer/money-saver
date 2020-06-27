e0e3de8cf17135f26a94947cb73275546163bf9b
savayer <sava0094@gmail.com>
2020-06-27 20:58:29 +0800

    (add) validate form of the category updating & fill name & limit fields after select a category

--- src/components/categories/CategoryUpdate.vue
+++ src/components/categories/CategoryUpdate.vue
@@ -2,27 +2,32 @@
   <div class="col s12 m6">
     <div>
 
-      <form>
+      <form @submit.prevent="updateCategory()">
         <div class="input-field">
-          <select id="select">
+          <select id="select" v-model="category" @change="selectCategory()">
             <option value="" disabled selected>Select a category</option>
-            <option v-for="category in categories" :value="category.id" :key="category.id">
+            <option v-for="category in categories" :value="category" :key="category.id">
               {{ category.name }}
             </option>
           </select>
           <label>Category</label>
-          <span class="helper-text invalid">Select a category</span>
+          <span v-if="$v.category.$dirty && !$v.category.required"
+            class="helper-text invalid">Select a category</span>
         </div>
         <div class="input-field">
           <input type="text" id="name" v-model="name"/>
           <label for="name">Name</label>
-          <span class="helper-text invalid">Enter name</span>
+          <span v-if="$v.name.$dirty && !$v.name.required"
+            class="helper-text invalid">Enter name</span>
         </div>
 
         <div class="input-field">
           <input id="limit" type="number" v-model="limit" />
           <label for="limit">Limit</label>
-          <span class="helper-text invalid">Max limit</span>
+          <span v-if="$v.limit.$dirty && !$v.limit.required"
+            class="helper-text invalid">Limit is required</span>
+          <span v-if="$v.limit.$dirty && !$v.limit.minValue"
+            class="helper-text invalid">Min limit {{ $v.limit.$params.minValue.min }} </span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -36,6 +41,7 @@
 
 <script>
 import M from 'materialize-css'
+import { required, minValue } from 'vuelidate/lib/validators'
 
 export default {
   name: "CategoryUpdate",
@@ -43,13 +49,38 @@ export default {
     return {
       name: '',
       limit: '',
-      selectInstance: null
+      category: null
     };
   },
   computed: {
     categories () {
       return this.$store.getters.categories
     }
+  },
+  methods: {
+    selectCategory () {
+      this.name = this.category.name
+      this.limit = this.category.limit
+      setTimeout(() => {
+        M.updateTextFields()
+      }, 0);
+    },
+    updateCategory () {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+
+    }
+  },
+  validations: {
+    name: { required },
+    limit: {
+      required,
+      minValue: minValue(100)
+    },
+    category: { required }
   },
   async mounted () {
     await this.$store.dispatch('getCategories')
